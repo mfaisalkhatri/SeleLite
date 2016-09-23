207390f78e0b54903a5a930a7bcf81fa7ca52b17
Mohammad Faisal Khatri <mohammadfaisalkhatri@gmail.com>
2016-09-23 22:45:36 +0530

    New Customer Page Objects and test cases
    Updated the Page Object Model for finding the New Customer Link in after
    Login and cases added

--- src/com/guru99/Pages/NewCustomerPage.java
+++ src/com/guru99/Pages/NewCustomerPage.java
@@ -1,16 +1,23 @@
 package com.guru99.Pages;
 
+
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+//import org.openqa.selenium.WebElement;
+
 
 public class NewCustomerPage {
 
-	public WebDriver driver;
+	private WebDriver driver;
+	
+	
 	
 	//By NewCustLink = By.xpath("html/body/div[2]/div/ul/li[2]/a");
-	By NewCustLink = By.tagName("NewCust");
+	//By NewCustLink = By.cssSelector("a[href = 'addcustomerpage.php']");
+	//By NewCustLink = By.linkText("New Customer");
 	
+	By NewCust = By.cssSelector("a[href='addcustomerpage.php']");
 	By CustName = By.name("name");
 	By CustRadioGenM = By.xpath("html/body/table/tbody/tr/td/table/tbody/tr[5]/td[2]/input[1]");
 	By CustRadioGenF = By.xpath("html/body/table/tbody/tr/td/table/tbody/tr[5]/td[2]/input[2]");
@@ -36,26 +43,32 @@ public class NewCustomerPage {
 	By CPin = By.xpath(".//*[@id='customer']/tbody/tr[11]/td[2]");
 	By CMobNo = By.xpath(".//*[@id='customer']/tbody/tr[12]/td[2]");
 	By CEmail = By.xpath(".//*[@id='customer']/tbody/tr[13]/td[2]");
-	By Contlink = By.xpath(".//*[@id='customer']/tbody/tr[14]/td[2]");
-			
+	//By Contlink = By.xpath(".//*[@id='customer']/tbody/tr[14]/td[2]");
+	By ContLink = By.cssSelector("a[href='Managerhomepage.php']");	
 	
+	public NewCustomerPage(WebDriver driver) {
+		// TODO Auto-generated constructor stub
+		this.driver = driver;
+		//parent = driver.findElement(By.tagName("body"));
+	}
 	
 	public void CustClick() {
-		driver.findElement(NewCustLink).click();
+		
+		driver.findElement(NewCust).click();
 		//System.out.println(link);
 	}
 	
 	public void AddCustomer () {
 		
-		driver.findElement(CustName).sendKeys("Ricky Richards");
+		driver.findElement(CustName).sendKeys("Ricky Rich");
 		driver.findElement(CustRadioGenM).click();
-		driver.findElement(CustDob).sendKeys("17/05/1991");
-		driver.findElement(CustAddress).sendKeys("Tanwar Complex, near Petrol Pump, Thane");
+		driver.findElement(CustDob).sendKeys("18/05/1991");
+		driver.findElement(CustAddress).sendKeys("Tanwar Complex near Petrol Pump Thane");
 		driver.findElement(CustCity).sendKeys("Mumbai");
 		driver.findElement(CustState).sendKeys("Maharashtra");
 		driver.findElement(CustPin).sendKeys("400003");
-		driver.findElement(CustMobNo).sendKeys("9821454578");
-		driver.findElement(CustEmail).sendKeys("ricky@gmail.com");
+		driver.findElement(CustMobNo).sendKeys("9820454578");
+		driver.findElement(CustEmail).sendKeys("ricky1@gmail.com");
 		driver.findElement(CustPass).sendKeys("Pass123");
 		
 	}
@@ -90,7 +103,7 @@ public class NewCustomerPage {
 	System.out.println("Customer Mobile No. is: " +CuMobNo);
 	System.out.println("Customer Email is: " +CuEmail);
 	
-	driver.findElement(Contlink).click();
+	driver.findElement(ContLink).click();
 	
 }
 
--- src/com/guru99/TestCases/NewCustomer.java
+++ src/com/guru99/TestCases/NewCustomer.java
@@ -1,8 +1,15 @@
 package com.guru99.TestCases;
 
-import java.io.IOException;
+//import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 import com.guru99.Pages.LoginPage;
@@ -11,29 +18,47 @@ import com.guru99.Pages.NewCustomerPage;
 public class NewCustomer {
 
 	public WebDriver driver;
+	public WebDriverWait wait;
+	
+	@BeforeSuite(alwaysRun = true)
+	public void BeforeLogin (){
+		driver = new FirefoxDriver();
+		wait = new WebDriverWait(driver,10);
+	}
+	
 	@Test 
 	public void NewCust () throws Exception {
-		LoginPage login = new LoginPage();
-		NewCustomerPage NCust = new NewCustomerPage();
+		LoginPage login = new LoginPage(driver);
+		NewCustomerPage NCust = new NewCustomerPage(driver);
 		
-		login.LoadSite();
+		driver.get(login.baseurl);
+		//login.LoadSite();
 		login.loginSite("mngr47682","suqyvag");
 		try {
-		String windhandle = driver.getWindowHandle();
+		//String windhandle = driver.getWindowHandle();
 		
-		driver.switchTo().window(windhandle);
+		//driver.switchTo().window(windhandle);
 			//System.out.println();
+		//Thread.sleep(9000);
+		/*WebElement newc= driver.findElement(By.cssSelector("a[href='addcustomerpage.php']"));
+		newc.click();*/
 		NCust.CustClick();
 		NCust.AddCustomer();
+		Thread.sleep(5000);
 		NCust.Submit();
 		NCust.AfterAdd();
 		
-		login.ClSite();
+		//login.ClSite();
 
 		}
 		catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
 				
+	}
+	
+	@AfterSuite(alwaysRun = true)
+	public void Teardown() {
+		driver.quit();
 	}
 }
